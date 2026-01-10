0763940406c75ff00886ffe1b4d1af51fc43c11b
paltan844 <info2bulletbazaar@gmail.com>
2026-01-10 18:17:42 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,55 +1,20 @@
+
 import React from "react";
+import { HelmetProvider } from "react-helmet-async";
 import Navigation from "./src/navigation/Navigation.web";
 
 const App = () => {
-  const isMobileOrTablet = window.innerWidth <= 1024;
-
-  if (!isMobileOrTablet) {
-    return (
-      <div style={styles.restrictedContainer}>
-        <h2 style={styles.title}>🚫 Desktop Not Supported</h2>
-        <p style={styles.message}>
-          Please open this app on your <strong>mobile</strong> or{" "}
-          <strong>tablet</strong> device.
-        </p>
-      </div>
-    );
-  }
-
   return (
-    <div style={styles.container}>
-
-      {/* App Navigation */}
-      <Navigation />
-    </div>
+    <HelmetProvider>
+      <div style={styles.container}>
+        <Navigation />
+      </div>
+    </HelmetProvider>
   );
 };
 
 const styles: Record<string, React.CSSProperties> = {
-  container: {
-    minHeight: "100vh",
-    backgroundColor: "#fff",
-  },
-  restrictedContainer: {
-    height: "100vh",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#f5f5f5",
-    color: "#333",
-    textAlign: "center",
-    padding: "20px",
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 700,
-    marginBottom: 8,
-  },
-  message: {
-    fontSize: 16,
-    color: "#555",
-  },
+  container: { minHeight: "100vh", backgroundColor: "#fff" },
 };
 
 export default App;
