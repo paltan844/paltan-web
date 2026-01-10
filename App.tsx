a8ec1cb57bab855f2f59fe884ee6625ca0a55cc8
paltan844 <info2bulletbazaar@gmail.com>
2026-01-10 18:21:24 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,15 +1,14 @@
 
 import React from "react";
-import { HelmetProvider } from "react-helmet-async";
 import Navigation from "./src/navigation/Navigation.web";
 
 const App = () => {
   return (
-    <HelmetProvider>
+    
       <div style={styles.container}>
         <Navigation />
       </div>
-    </HelmetProvider>
+    
   );
 };
 
