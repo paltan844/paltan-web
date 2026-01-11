a9f5a5ee3997c2a221b1973feceda60ad3f31b6b
paltan844 <info2bulletbazaar@gmail.com>
2026-01-12 00:39:48 +0530

    Update ga4Tracker.tsx

--- src/analytics/ga4Tracker.tsx
+++ src/analytics/ga4Tracker.tsx
@@ -1,3 +1,4 @@
+/*
 import { useEffect } from "react";
 import { navigationRef } from "@utils/NavigationUtils";
 
@@ -39,3 +40,42 @@ const GA4Tracker = () => {
 };
 
 export default GA4Tracker;
+*/
+
+
+
+
+import { useEffect } from "react";
+import { navigationRef } from "@utils/NavigationUtils";
+
+declare global {
+  interface Window {
+    gtag?: (...args: any[]) => void;
+  }
+}
+
+const GA_TRACKING_ID = "G-7WB75XS340"; // 🔴 apni GA4 ID daalo
+
+const GA4Tracker = () => {
+  useEffect(() => {
+    if (!navigationRef) return;
+
+    const unlisten = navigationRef.listen(({ location }) => {
+      if (window.gtag) {
+        window.gtag("config", GA_TRACKING_ID, {
+          page_path: location.pathname + location.search,
+        });
+      }
+    });
+
+    return () => {
+      if (unlisten) unlisten();
+    };
+  }, []);
+
+  return null;
+};
+
+export default GA4Tracker;
+
+
